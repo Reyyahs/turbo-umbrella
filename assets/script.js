3e33d066751f55e7c7d3e5728f9e538f22072de0
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-06-12 22:37:41 -0400

    added a 'prompt() method

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
-// Assignment code here
 function generatePassword() {
   var numericCharacters = "0123456789";
   var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
   var specialCharacters = "!@#$%^&*()_[]{}<>?/";
   var possibleCharacters = "";
+//use to validate and ensure that the user enters a valid length for the password
+  var numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128 || isNaN(numberOfCharacters)) {
+    return "Choose a valid number of characters.";
+  }
 }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
